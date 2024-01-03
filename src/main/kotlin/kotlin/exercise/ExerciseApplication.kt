4cc480b190846c91cc35b6f7fdec9164098c6ce0
Jacob Kim <jacob.kim@aidkr.com>
2024-01-03 15:01:24 +0900

    initial commit

---
+++ src/main/kotlin/kotlin/exercise/ExerciseApplication.kt
@@ -0,0 +1,11 @@
+package kotlin.exercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExerciseApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExerciseApplication>(*args)
+}
---
+++ src/test/kotlin/kotlin/exercise/ExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlin.exercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExerciseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
